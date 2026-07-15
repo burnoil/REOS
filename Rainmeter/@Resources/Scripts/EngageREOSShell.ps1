e9b2e4faf031a07ea44aed5e6750aeb361f442f3
burnoil <tekdr@tekdr.net>
2026-07-15 09:04:05 -0700

    WO-001: add optional REOS shell engagement script

---
+++ Rainmeter/@Resources/Scripts/EngageREOSShell.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+Add-Type -TypeDefinition @'
+using System;
+using System.Runtime.InteropServices;
+public static class REOSWindowsShell {
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindow(string className, string windowName);
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr handle, int command);
+}
+'@
+
+$taskbar = [REOSWindowsShell]::FindWindow('Shell_TrayWnd', $null)
+if ($taskbar -ne [IntPtr]::Zero) {
+    [REOSWindowsShell]::ShowWindow($taskbar, 0) | Out-Null
+}
+
+$advanced = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty -Path $advanced -Name HideIcons -Value 1
+
+Stop-Process -Name explorer -Force
+Start-Sleep -Milliseconds 500
+Start-Process explorer.exe
